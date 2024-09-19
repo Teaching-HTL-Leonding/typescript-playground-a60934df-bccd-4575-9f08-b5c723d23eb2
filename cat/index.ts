39714eea17c647daf6894790d1d0e748ca2e19c5
Rainer Stropek <rainer@timecockpit.com>
2024-09-19 09:44:34 +0200

    Saved in TypeScript Playground at 2024-09-19T07:44:33.586Z

---
+++ cat/index.ts
@@ -0,0 +1,23 @@
+function setup() {
+    // create the canvas (German: "Leinwand")
+    // Paramters are width and height
+    createCanvas(1000, 1000)
+
+    // Set background color
+    background("cyan")
+
+    // Draw a circle
+    fill("Lightgray")
+    circle(350, 350, 300)
+
+    // Draw a circle
+    fill("black")
+    circle(410, 320, 30)
+
+    // Draw a circle
+    fill("black")
+    circle(290, 320, 30)
+
+// Draw a triangle
+triangle(300, 200, 200,300, 200, 350)
+}
