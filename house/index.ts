0fd0606679f4aed92a2963113821dd61e24ee4e2
Rainer Stropek <rainer@timecockpit.com>
2024-09-19 08:42:21 +0200

    Saved in TypeScript Playground at 2024-09-19T06:42:21.020Z

---
+++ house/index.ts
@@ -0,0 +1,13 @@
+function setup() {
+    // create the canvas (German: "Leinwand")
+    // Paramters are width and height
+    createCanvas(1000, 1000);
+
+    // Set background color
+    background("Deepskyblue");
+
+    // test
+
+    // Draw a rectangle 
+    rect(200, 200, 120, 200)
+}
