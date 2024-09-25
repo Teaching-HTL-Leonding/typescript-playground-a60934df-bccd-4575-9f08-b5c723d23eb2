032349522c5ce1759529f58aa42ddd387bc81fc9
Rainer Stropek <rainer@timecockpit.com>
2024-09-25 22:03:35 +0200

    Saved in TypeScript Playground at 2024-09-25T20:03:35.103Z

--- robot/index.ts
+++ robot/index.ts
@@ -1,6 +1,17 @@
 function setup() {
     createCanvas(400, 400)
     background("lightgray")
-     rect(80, 120, 250)
-     rect(80, 122, )
+
+    fill("darkgray")
+    rect(80, 140, 240)
+
+    fill("white")
+    rect(125, 300, 150, 30)
+
+fill("black")
+    circle(120, 200, 50)
+    circle(280, 200, 50)
+    fill("white")
+    circle(200, 60, 20)
+line(200, 140, 200, 70, )
 }
