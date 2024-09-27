08dd52b7df71e266ea059342a179eef2a57f787b
Rainer Stropek <rainer@timecockpit.com>
2024-09-27 12:36:56 +0200

    Saved in TypeScript Playground at 2024-09-27T10:36:56.034Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -4,14 +4,16 @@ function setup() {
 
 }
 let x: number = 0
-let direction: number = +1
+let direction: number = +3
 
 function draw(){
 background("black")
 
 x = x + direction
 if (x >=width){
-    direction = -1
+    direction = -3
+}else if (x <=0){
+    direction= +3
 }
 circle(x, height / 2, 50)
 }
