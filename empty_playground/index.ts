a8b44bc6a7b12a3308222a92442c86ca81322289
Rainer Stropek <rainer@timecockpit.com>
2024-09-27 12:21:06 +0200

    Saved in TypeScript Playground at 2024-09-27T10:21:05.645Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -8,4 +8,8 @@ let text3 = `${text1} ${text2}`
 text3 = `${text3}!`
 text(text3, 50, 50)
 
+
+const longtext= `Das ist langeeeeeeer texthhhhhhhhhhhhhhhwwwwwwwwiiiiihhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh`
+text(longtext, 0, 200, 400, 400)
+
 }
