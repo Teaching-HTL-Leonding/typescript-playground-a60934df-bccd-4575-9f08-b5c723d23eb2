9095f3d6501240bfa571b7626282efc97a5c37af
Rainer Stropek <rainer@timecockpit.com>
2024-10-17 08:40:01 +0200

    Saved in TypeScript Playground at 2024-10-17T06:40:01.322Z

--- digits/index.ts
+++ digits/index.ts
@@ -34,13 +34,19 @@ function setup() {
   stroke("yellow")
   rect(480, 150, 70, 100)
 
-  textAlign(CENTER, CENTER);
-  fill("yellow");
-  noStroke();
-  text(num, width / 2, height - 20);
+  textAlign(CENTER, CENTER)
+  fill("yellow")
+  noStroke()
+  text(num, width / 2, height - 20)
 
-  textAlign(CENTER, CENTER);
-  fill("yellow");
-  noStroke();
-  text(Math.floor(num % 10), width / 1.13, height - 200);
+  textAlign(CENTER, CENTER)
+  fill("yellow")
+  noStroke()
+ textSize(100)
+  text(Math.floor(num  % 10), width / 1.13, height - 190)
+  text(Math.floor(num/10  % 10), width / 1.35, height - 190)
+  text(Math.floor(num/100 % 10), width / 1.7, height - 190)
+  text(Math.floor(num/1000 % 10), width / 2.4, height - 190)
+  text(Math.floor(num/10000 % 10), width / 3.7, height - 190)
+  text(Math.floor(num/100000 % 10), width / 8, height - 190)
 }
