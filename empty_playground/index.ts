3cccefcdadb6968822e56a8ffc6a08e9f7621fa9
Rainer Stropek <rainer@timecockpit.com>
2024-11-05 13:57:10 +0100

    Saved in TypeScript Playground at 2024-11-05T12:57:09.581Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -1,19 +1,5 @@
 function setup() {
-    createCanvas(400, 400)
-
-
 }
-let x: number = 0
-let direction: number = +3
 
-function draw(){
-background("black")
-
-x = x + direction
-if (x >=width){
-    direction = -3
-}else if (x <=0){
-    direction= +3
-}
-circle(x, height / 2, 50)
+function draw() {
 }
