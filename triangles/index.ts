432b1c2dcc569572470c1e417e440b075f76453b
Rainer Stropek <rainer@timecockpit.com>
2024-11-28 09:34:21 +0100

    Saved in TypeScript Playground at 2024-11-28T08:34:21.486Z

---
+++ triangles/index.ts
@@ -0,0 +1,19 @@
+function setup() {
+    const SIZE = 400;
+    const SIDE_LENGTH = 50;
+
+    // We have an isosceles triangle ("gleichseitiges Dreieck").
+    // This is the formula to calculate the height of such a triangle
+    const HEIGHT = SIDE_LENGTH * Math.sqrt(3) / 2;
+
+    createCanvas(SIZE, HEIGHT * 9);
+    background("black");
+
+    strokeWeight(1);
+    stroke("yellow");
+    noFill();
+
+for (let i )
+
+
+}
