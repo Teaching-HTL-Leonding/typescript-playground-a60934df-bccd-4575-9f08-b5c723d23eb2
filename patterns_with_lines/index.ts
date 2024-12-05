1136848e3c3de9b8a966f21dc4910f0edc1ef9a3
Rainer Stropek <rainer@timecockpit.com>
2024-12-05 09:00:32 +0100

    Saved in TypeScript Playground at 2024-12-05T08:00:31.653Z

--- patterns_with_lines/index.ts
+++ patterns_with_lines/index.ts
@@ -26,6 +26,7 @@ function mouseMoved() {
     for (let x = 0; x < 601; x++) {
         for (let i = 0; i < 50; i += 1) {
             line(0, height1, 10, height2)
+            line(10,height2,0, height1 + 10)
             height1 += 10
             height2 += 10
         }
@@ -34,17 +35,5 @@ function mouseMoved() {
         height2 = 5
     }
 
-    let height3 = 10
-    let height4 = 15
-
-    for (let j = 0; j < 601; j++) {
-        for (let k = 0; k < 50; k += 1) {
-            line(10, height4, 0, height3)
-            height3 += 10
-            height4 += 10
-        }
-        translate(10,0)
-        height3 = 10
-        height4 = 10
-    }
+   
 }
