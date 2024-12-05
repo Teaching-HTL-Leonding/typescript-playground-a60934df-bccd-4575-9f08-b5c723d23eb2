09f06bc43b952b2313c43ccdb2083dcb5136853b
Rainer Stropek <rainer@timecockpit.com>
2024-12-05 08:52:47 +0100

    Saved in TypeScript Playground at 2024-12-05T07:52:47.410Z

--- patterns_with_lines/index.ts
+++ patterns_with_lines/index.ts
@@ -2,7 +2,7 @@ const SIZE = 10
 function setup() {
     createCanvas(601, 301);
     background("black");
-
+colorMode(HSB)
     fill("aqua");
     noStroke();
 
@@ -29,17 +29,22 @@ function mouseMoved() {
             height1 += 10
             height2 += 10
         }
-translate(10,0)
-height1 = 0
-height2= 5
+        translate(10, 0)
+        height1 = 0
+        height2 = 5
     }
 
-    let height3 = 0
-    let height4 = 5
+    let height3 = 10
+    let height4 = 15
 
-    for ( let j = 0; j < 601; j++){
-        for (let k = 0; k < 50; k++){
-            line(10,height4,0,height3)
+    for (let j = 0; j < 601; j++) {
+        for (let k = 0; k < 50; k += 1) {
+            line(10, height4, 0, height3)
+            height3 += 10
+            height4 += 10
         }
+        translate(10,0)
+        height3 = 10
+        height4 = 10
     }
 }
